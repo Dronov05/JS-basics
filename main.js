9adaf287080efc6210716663eee01ebe07164c17
Max <23432-az@mail.ru>
2023-05-24 15:56:23 +0300

    Добавил проект

---
+++ main.js
@@ -0,0 +1,93 @@
+'use strict'
+
+//***  1 Переменные
+
+
+
+// const firstName = 'Maxim'
+// const lastName = 'Dronov'
+// let age = 32
+
+//***  2 Мутирование
+
+// console.log('Имя человека: ' + firstName + ', а возраст человека: ' + age)
+
+// const sum = prompt('Введите число')
+// alert('Вы ввели число: ' + sum)
+
+
+//*** 3 Операторы
+
+// const currentYear = 2023
+// const birthYear = 1991
+
+// const age = currentYear - birthYear
+// console.log(age)
+
+// let a = 10
+// let b = 5
+// let c = a + b
+// console.log(c)
+// c += a
+// console.log(c)
+
+
+//*** 4 Типы данных
+
+// const isProgrammer = true
+// const name = 'Max'
+// const age = 32
+// let x
+
+// console.log(typeof isProgrammer)
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof x)
+
+
+//*** 5 Приоритет операторов
+// const fullAge = 32
+// const birthYear = 1991
+// const currentYear = 2023
+
+// const isFullAge = currentYear - birthYear >= fullAge
+// console.log(isFullAge)
+
+
+
+//*** 6 Условные операторы
+
+// const courseStatus = 'fail' // ready, fail, pending
+
+// if (courseStatus === 'ready') {
+//     console.log('Курс уже готов и его можно проходить')
+// } else if (courseStatus === 'pending') {
+//     console.log('Курс находится в разработке')
+// } else {
+//     console.log('Курс не получился')
+// }
+
+// const isReady = false
+
+// if (isReady) {
+//     console.log('Всё готово!')
+// } else {
+//     console.log('Всё не готово!')
+// }
+
+//* Тернарное выражение
+// isReady ? console.log('Всё готово!') : console.log('Всё не готово!')
+
+
+// const num1 = 42
+// const num2 = '42'
+// console.log(num1 == num2)
+// console.log(num1 === num2) //Рекомендуется использовать тройное равно ===
+
+
+//*** 7 Булевая логика
+
+
+
+//*** 8 Функции 1:22:54 
+
